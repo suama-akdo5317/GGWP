eec4f8c70fded4e25e4ba80364b8b13879ca8780
Ubuntu <ubuntu@ip-172-31-50-90.ec2.internal>
2021-02-10 15:09:20 +0000

    post list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def show
+    @posts = current_user.posts.order(id: :desc).page(params[:page]).per(12)
   end
   
 end
