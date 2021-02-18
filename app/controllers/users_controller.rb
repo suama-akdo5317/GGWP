e0e85417a672fad69735db9cec08a1f6f39aa63b
Ubuntu <ubuntu@ip-172-31-50-90.ec2.internal>
2021-02-18 17:01:51 +0000

    create post_page & user_page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    counts(@user)
+    # counts(@user)
   end
   
   def update
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find(params[:id])
+    @user = User.find_by(id: params[:id])
   end
   
 end
