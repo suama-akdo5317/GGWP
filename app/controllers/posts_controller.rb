9584b54e30f137ab284d015c582cdcb11328b7f4
Ubuntu <ubuntu@ip-172-31-50-90.ec2.internal>
2021-02-18 08:12:25 +0000

    post image

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -54,7 +54,7 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :image)
+    params.require(:post).permit(:title, :content, image: [])
   end
   
   def correct_user
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   private
 
   def set_user
-    @user = User.find([:id])
+    @user = User.find(params[:id])
   end
   
 end
