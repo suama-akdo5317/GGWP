3ac5056fc4d36d9e2bd7df58ee9f7dd53069e308
Ubuntu <ubuntu@ip-172-31-50-90.ec2.internal>
2021-02-20 07:40:43 +0000

    avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,24 @@ class User < ApplicationRecord
   has_many :posts
   
   has_one_attached :avatar
+  
+  validate :validate_avatar
+    # アバター画像のバリデーション内容
+  def validate_avatar
+    return unless avatar.attached?
+    # 画像サイズの制限
+    if avatar.blob.byte_size > 10.megabytes
+      # エラーメッセージはi18nから取得
+      errors.add(:avatar, :file_too_large)
+    elsif !image?
+      errors.add(:avatar, :file_type_not_image)
+    end
+  end
+
+  # 拡張子でファイルの種類を確認
+  def image?
+    avatar.content_type.in?(%("image/jpeg image/jpg image/png"))
+  end
 
 
 end
