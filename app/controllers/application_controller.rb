d61b0596dd17a389ff3b9f1c4b132d528fe4bf23
Ubuntu <ubuntu@ip-172-31-50-90.ec2.internal>
2021-02-22 14:40:43 +0000

    update layouts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,9 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
       devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+    # deviseのpermitted_parameterを追加する
+      devise_parameter_sanitizer.permit(:account_update, keys: [:avatar] )
+
     end
 
   def after_sign_in_path_for(resource)
