8ccc708d12ea46d29ce83047413e845b9bfddd45
Ubuntu <ubuntu@ip-172-31-50-90.ec2.internal>
2021-03-17 06:41:54 +0000

    setting sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,7 +98,7 @@ Rails.application.configure do
   :enable_starttls_auto => true,
   :address => "smtp.sendgrid.net",
   :port => 587,
-  :domain => "heroku.com",
+  :domain => "herokuapp.com",
   :authentication => :plain,
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
